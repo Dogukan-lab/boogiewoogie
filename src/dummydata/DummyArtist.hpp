5e0482b2e7ffe9e2bfd486efae966faabcebf801
Dogukan-lab <57491366+Dogukan-lab@users.noreply.github.com>
2024-10-21 17:33:41 +0200

    [ADD] Added GLM library for simple 2D calculations and such

---
+++ src/dummydata/DummyArtist.hpp
@@ -0,0 +1,19 @@
+//
+// Created by doguk on 10/21/2024.
+//
+
+#ifndef DUMMYARTIST_HPP
+#define DUMMYARTIST_HPP
+
+class Artist {
+public:
+    Artist();
+
+    Artist();
+
+private:
+
+};
+
+
+#endif //DUMMYARTIST_HPP
